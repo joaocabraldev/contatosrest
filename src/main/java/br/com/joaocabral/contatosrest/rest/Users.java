5768e7113c605d4ca27b5f14b7437966f756995b
João Antônio Cabral <joaomunizshura@gmail.com>
2016-06-13 08:51:35 -0300

    FindByLogin

--- src/main/java/br/com/joaocabral/contatosrest/rest/Users.java
+++ src/main/java/br/com/joaocabral/contatosrest/rest/Users.java
@@ -1,13 +1,22 @@
 package br.com.joaocabral.contatosrest.rest;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import br.com.joaocabral.contatosrest.model.SystemUser;
 
+/**
+ * Repositório de Usuários.
+ * Ofereçe opções de Paginação e Ordenação automaticamente.
+ */
 @RepositoryRestResource
 public interface Users extends PagingAndSortingRepository<SystemUser, Long> {
 
-	
+	/**
+	 * Busca um Usuário por Login.
+	 * @param login Login a ser pesquisado.
+	 */
+	SystemUser findOneByLogin(@Param("login") login);
 	
 }
