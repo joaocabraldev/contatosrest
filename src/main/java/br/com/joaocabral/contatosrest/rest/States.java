3c793bc41167bc4b5d020aa5127b61f421e66174
João Antônio Cabral <joaomunizshura@gmail.com>
2016-06-13 08:53:37 -0300

    Javadoc

--- src/main/java/br/com/joaocabral/contatosrest/rest/States.java
+++ src/main/java/br/com/joaocabral/contatosrest/rest/States.java
@@ -5,6 +5,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import br.com.joaocabral.contatosrest.model.State;
 
+/**
+ * Reposítório de Estados.
+ * Ofereçe opções de Paginação e Ordenação automaticamente.
+ */
 @RepositoryRestResource
 public interface States extends PagingAndSortingRepository<State, Long> {
 
